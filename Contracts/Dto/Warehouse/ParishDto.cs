9a2f747c070db4ad653e6b177a2910635c29af10
SinaRiyahi <sina.riyahi76@gmail.com>
2023-08-02 15:17:01 +0330

    Parish

--- Contracts/Dto/Warehouse/ParishDto.cs
+++ Contracts/Dto/Warehouse/ParishDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Contracts.Dto.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Contracts.Dto.Warehouse
 {
-    internal class ParishDto
+    public class ParishDto : BaseDto<int>
     {
+        public string ParishName { get; set; }
     }
 }
